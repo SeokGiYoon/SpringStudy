171554d9f2371772f7847ab16b25f9a9501a06d9
SeokGiYoon <evertys2004@gmail.com>
2019-02-21 00:50:13 +0900

    Entity에 getter가 없으면 mustache에서 값을 가져올 수 없음

--- src/main/java/net/seokgi/MySeokgiApplication.java
+++ src/main/java/net/seokgi/MySeokgiApplication.java
@@ -1,14 +1,23 @@
 package net.seokgi;
 
+import com.samskivert.mustache.Mustache;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mustache.MustacheEnvironmentCollector;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
+
 
 @SpringBootApplication
 public class MySeokgiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MySeokgiApplication.class, args);
+		
+		
 	}
+	
 
 }
 
--- src/main/java/net/seokgi/domain/User.java
+++ src/main/java/net/seokgi/domain/User.java
@@ -36,6 +36,30 @@ public class User {
 	public void setEmail(String email) {
 		this.email = email;
 	}
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
 
 	@Override
 	public String toString() {
