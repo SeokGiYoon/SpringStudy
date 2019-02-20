fd5c59b2f838b3f1fbd6d3093c4f97e97d096584
SeokGiYoon <evertys2004@gmail.com>
2019-02-21 00:52:46 +0900

    불필요한 import제거

--- src/main/java/net/seokgi/MySeokgiApplication.java
+++ src/main/java/net/seokgi/MySeokgiApplication.java
@@ -1,12 +1,7 @@
 package net.seokgi;
 
-import com.samskivert.mustache.Mustache;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.mustache.MustacheEnvironmentCollector;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.core.env.Environment;
 
 
 @SpringBootApplication
